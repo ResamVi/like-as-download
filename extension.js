b8df1c321de7e0ccdf1b616063312206fac25baf
Julien Midedji <Julien.Midedji@gmail.com>
2018-11-03 15:06:21 +0100

    Add firefox extension client

---
+++ extension.js
@@ -0,0 +1,29 @@
+console.log('like-as-download started.');
+
+function onStarted(id) {
+  console.log('Started downloading: ' + id);
+}
+
+function onFailed(error) {
+  console.log('Download failed: ${error}');
+}
+
+function logResponse(responseDetails) {
+  const videoUrl = responseDetails.documentUrl;
+  const videoId = videoUrl.slice("https://www.youtube.com/watch?v=".length);
+
+  let downloading = browser.downloads.download({
+    url: 'http://localhost:8080/' + videoUrl,
+    filename : videoId + '.mp4',
+    conflictAction : 'uniquify',
+  });
+  
+  downloading.then(onStarted, onFailed );
+}
+
+browser.webRequest.onCompleted.addListener(
+  logResponse,
+  {
+    urls: ['https://www.youtube.com/service_ajax?name=likeEndpoint']
+  }
+);
