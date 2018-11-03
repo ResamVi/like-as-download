a50dca01a2caaa0a1b2b831423194d0849e1100b
Julien Midedji <Julien.Midedji@gmail.com>
2018-11-03 13:20:44 +0100

    Serve requests

--- index.js
+++ index.js
@@ -2,33 +2,41 @@ const fs = require('fs');
 const youtubedl = require('youtube-dl');
 const http = require('http');
 
-// video.pipe(fs.createWriteStream(info.filename));
-
 http.createServer(function (req, res) {
   
   if(req.url === '/favicon.ico') {
     return;
   }
-
-  const url = req.url.slice(1);
-  console.log('Request: ' + url);
   
   // format 18 is mp4
-  var video = youtubedl(url, ['--format=18'], { cwd: __dirname });
-
+  const url = req.url.slice(1);
+  const video = youtubedl(url, ['--format=18'], { cwd: __dirname });
+  let filename;
+  
   video.on('info', function(info) {
-    console.log('Download started');
-    console.log('filename: ' + info._filename);
-    console.log('size: ' + info.size);
-  });
-
-  video.on('end', function(info) {
-    console.log('Download finished');
-    res.writeHead(200, {'Content-Type': 'video/mp4'});
-    res.write(req.url);
-    res.end();
+    console.log('Download started:\t' + url);
+    console.log('filename:\t\t' + info._filename);
+    console.log('size:\t\t\t' + info.size);
+    filename = info._filename;
+    video.pipe(fs.createWriteStream(info._filename));
   });
   
+  video.on('end', function(info) {
+    console.log('Download finished');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    fs.readFile('./' + filename, function(error, content) {
+      if(error) {
+        console.log(error);
+      } else {
+        res.writeHead(200, { 
+          'Content-Type': 'video/mp4',
+          'Content-disposition': 'attachment; filename="' + filename + '"',
+        });
+        res.end(content);
+      }
+    });
+  });
 }).listen(8080);
 
 console.log('Server started');
